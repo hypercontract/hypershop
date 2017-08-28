dc330ec974e5b293506a7595e904b8393616f267
Marvin Luchs <marvin.luchs@t-systems.com>
2017-08-28 23:19:47 +0200

    use product URI for shopping cart item creation

--- shoppingCart/router.js
+++ shoppingCart/router.js
@@ -3,6 +3,7 @@ const config = require('config');
 const hal = require('./hal');
 const shoppingCartService = require('./service');
 const { getBasePath, getRootPath, getRootUri, getShoppingCartItemsPath, getShoppingCartItemPath } = require('./uris');
+const { getProductUri } = require('../products/uris');
 const { sendResponse } = require('../shared/util');
 
 const router = express.Router();
@@ -16,8 +17,17 @@ router.get(getRootPath(), (request, response) => {
 });
 
 router.post(getShoppingCartItemsPath(), (request, response) => {
+    let product;
+
+    // TODO: extract into separate method
+    if (request.get('Content-Type') === config.app.mediaType) {
+        product = request.body.product.replace(new RegExp(getProductUri('(.*)')), '$1');
+    } else {
+        product = request.body.product;
+    }
+
     shoppingCartService.addShoppingCartItem(
-        request.body.product,
+        product,
         request.body.quantity
     )
         .then(() => response.redirect(201, getRootUri()));
