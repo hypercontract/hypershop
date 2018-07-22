9f47bd6d34896ff42b859d7a5fde7988e29b35c1
Marvin Luchs <marvin.luchs@holisticon.de>
2018-07-22 22:45:37 +0200

    style: order imports

--- endpoints.ts
+++ endpoints.ts
@@ -1,9 +1,9 @@
-import * as profile from './profile/router';
+import { RequestHandler } from 'express';
+import * as orders from './orders/router';
 import * as products from './products/router';
+import * as profile from './profile/router';
 import * as shoppingCart from './shoppingCart/router';
 import * as userProfile from './userProfile/router';
-import * as orders from './orders/router';
-import { RequestHandler } from 'express';
 
 export interface Endpoint {
     basePath: string;
