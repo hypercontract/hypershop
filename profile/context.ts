8f245a0510591a5eb4780dc93be2671521b0d380
Marvin Luchs <marvin.luchs@holisticon.de>
2018-09-19 13:16:17 +0200

    config: change hypercontract namespace to https

--- profile/context.ts
+++ profile/context.ts
@@ -5,7 +5,7 @@ export const vocabularyContext = {
     rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
     rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
     xsd: 'http://www.w3.org/2001/XMLSchema#',
-    hyper: 'http://hypercontract.org/'
+    hyper: 'https://hypercontract.org/'
 };
 
 export const domainContext = {
